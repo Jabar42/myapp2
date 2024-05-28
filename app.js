ff36f28d982328f458978615954967f4fb022108
jaime barbudo <jaimebarbudomier@gmail.com>
2024-05-28 02:46:58 +0000

    Hola mundo funcionando correctamente

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const indexRouter = require('./routes/index');
+
 // Configura la carpeta "public" para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Usa las rutas definidas
+app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
